c56a4c2fe2bada478d6fcf6a33ae68483201d6b4
Md. Tajminur Rahman <tajminur.aiub@gmail.com>
2022-01-02 20:35:32 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config();
+//const admin = require("firebase-admin");
+const ObjectId = require('mongodb').ObjectId;
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7xlcz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('travail-app');
+        const usersCollection = database.collection('users');
+        // POST: save user to db
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            console.log(result);
+            res.json(result);
+        });
+
+        //PUT: Update user
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        });
+
+
+    }
+    finally {
+
+    }
+
+}
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+    res.send('Ah, here We Go!')
+})
+
+app.listen(port, () => {
+    console.log('Listening at: ', port)
+})
