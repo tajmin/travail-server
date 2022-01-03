8d8fca793946b492ad21933edc3178861c881016
Md. Tajminur Rahman <tajminur.aiub@gmail.com>
2022-01-03 15:05:02 +0600

    PORT change

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const ObjectId = require('mongodb').ObjectId;
 
 const app = express();
-const port = 2222 || process.env.PORT;
+const port = process.env.PORT || 2222;
 
 app.use(cors());
 app.use(express.json());
