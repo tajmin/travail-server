70f1f0c0cf33e81638b09ca75c4206912bfdb330
Mostofareza3 <86896410+Mostofareza3@users.noreply.github.com>
2022-01-03 10:41:48 +0600

    create new job

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const ObjectId = require('mongodb').ObjectId;
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 2222 || process.env.LOCAL;
 
 app.use(cors());
 app.use(express.json());
@@ -19,6 +19,7 @@ async function run() {
         await client.connect();
         const database = client.db('travail-app');
         const usersCollection = database.collection('users');
+        const jobCollection = database.collection('jobs');
         // POST: save user to db
         app.post('/users', async (req, res) => {
             const user = req.body;
@@ -36,6 +37,16 @@ async function run() {
             const result = await usersCollection.updateOne(filter, updateDoc, options);
             res.json(result);
         });
+
+        //POST: a new job
+        app.post('/addNewJob',async(req, res)=>{
+            const newJob = req.body;
+            console.log(newJob);
+            const result = await jobCollection.insertOne(newJob);
+            console.log(result)
+            res.json(result)
+        });
+
 
 
     }
